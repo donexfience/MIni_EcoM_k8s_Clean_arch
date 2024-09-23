de6923299df75de1c6c618415c2ff6cf5697b395
donexfience <donex6fience@gmail.com>
2024-09-23 18:53:03 +0530

    databse + ENV added

--- auth-service/src/config/database/database.ts
+++ auth-service/src/config/database/database.ts
@@ -6,4 +6,5 @@ export const dbConfig = {
   host: process.env.MONGO_URI || "localhost",
   port: Number(process.env.DB_PORT) || 27017,
   database: process.env.DB_NAME || "Authdatabase",
+  uri:`mongodb+srv://donex6fience:${process.env.MONGO_CLUSTER_PASS}@auth.bbdo8.mongodb.net/?retryWrites=true&w=majority&appName=AUTH`
 };
--- auth-service/src/infrastructure/repositories/mongodb/connection/connetion.ts
+++ auth-service/src/infrastructure/repositories/mongodb/connection/connetion.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 import { dbConfig } from "../../../../config/database/database";
 
-
 export class Database {
   public static async connect(): Promise<void> {
-    const { host, port, database } = dbConfig;
-    const connectionString = `mongodb://${host}:${port}/${database}`;
+    const { host, port, database, uri } = dbConfig;
+    const connectionString = uri || `mongodb://${host}:${port}/${database}`;
 
     try {
       await mongoose.connect(connectionString);
